abbc39f5e1ee712587dfec855466dc519a69efe5
Emiko Bell <emikoelizabethbell@gmail.com>
2023-04-10 08:34:59 -0700

    Completed wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,22 @@
 """Count words in file."""
 
 
-# put your code here.
+def count_words_in_file(input_file):
+    words_file = open(input_file)
+    words_in_file = {}
+
+    for line in words_file:
+        words = line.rstrip().split(" ")
+        for word in words:
+            words_in_file[word] = words_in_file.get(word, 0) + 1
+
+    return words_in_file
+
+def print_word_report(word_dictionary):
+    for word, count in word_dictionary.items():
+        print(word, count)
+
+
+word_dictionary = count_words_in_file('twain.txt')
+# word_dictionary = count_words_in_file('test.txt')
+print_word_report(word_dictionary)
